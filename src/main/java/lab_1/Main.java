eedf6f92696bc4e1ad2fd9effc008fc438ed9176
ewepu <150716285+ewepus@users.noreply.github.com>
2024-09-12 21:35:04 +0300

    init commit

---
+++ src/main/java/lab_1/Main.java
@@ -0,0 +1,73 @@
+package lab_1;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final String initialAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя .,?!-";
+    private static final String endAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~@#$%^&*()_+={}[]/|";
+
+    public static String encryption(String input) {
+        if (input == null || input.isBlank()) {
+            System.out.println("invalid input");
+        } else {
+            for (int i = 0; i < input.length(); i++) { //проверка на знакомость символов
+                String symbol = String.valueOf(input.charAt(i));
+                if (!initialAlphabet.contains(symbol)) {
+                    System.out.println("Незнакомый символ");
+                    return null;
+                }
+            }
+            String cryptogramm = "";
+            for (int i = 0; i < input.length(); i++) {
+                String symbol = String.valueOf(input.charAt(i));
+                int index = initialAlphabet.indexOf(symbol);
+                cryptogramm += endAlphabet.charAt(index);
+            }
+            return cryptogramm;
+        }
+        return null;
+    }
+    public static String decryption(String input) {
+        if (input == null || input.isBlank()) {
+            System.out.println("invalid input");
+        } else {
+            for (int i = 0; i < input.length(); i++) { //проверка на знакомость символов
+                String symbol = String.valueOf(input.charAt(i));
+                if (!endAlphabet.contains(symbol)) {
+                    System.out.println("Незнакомый символ");
+                    return null;
+                }
+            }
+            String initText = "";
+            for (int i = 0; i < input.length(); i++) {
+                String symbol = String.valueOf(input.charAt(i));
+                int index = endAlphabet.indexOf(symbol);
+                initText += initialAlphabet.charAt(index);
+            }
+            return initText;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Зашифровать сообщение - введите 1\n" +
+                    "Расшифровать сообщение - введите 2");
+            int numInput = Integer.parseInt(sc.nextLine());
+            System.out.println("Введите сообщение");
+            String input = sc.nextLine();
+
+            if (numInput == 1) {
+                System.out.println("Криптограмма: " + encryption(input));
+
+            } else if (numInput == 2) {
+                System.out.println("Расшифровка: " + decryption(input));
+            } else {
+                System.out.println("invalid input");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
